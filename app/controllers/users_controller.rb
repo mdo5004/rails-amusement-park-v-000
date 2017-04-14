636e5a82be94edff684783fa3781590d3df417e3
Michael O'Connell <michaeldavidoconnell@gmail.com>
2017-04-14 17:36:05 -0400

    Passing features signup specs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 require "pry"
 class UsersController < ApplicationController
-
+rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
     before_action :authenticate_user!, only: [:index, :show, :edit, :update, :destroy]
     def new
         @user = User.new
@@ -23,7 +23,6 @@ class UsersController < ApplicationController
     def show
         @user = User.find(params[:id])
         authorize @user
-        binding.pry
     end
     def edit
         @user = User.find(params[:id])     
@@ -40,10 +39,16 @@ class UsersController < ApplicationController
     
     private
     def user_params
-        params.require(:user).permit(:name, :password, :height, :tickets, :nausea, :happiness)
+        params.require(:user).permit(:name, :password, :height, :tickets, :nausea, :happiness, :admin)
     end
     
     def authenticate_user!
-        session[:user_id] 
+        if !session[:user_id]
+            redirect_to root_path, :alert => "Access denied."
+        end
+    end
+    
+    def user_not_authorized
+        redirect_to users_path, :alert => "Access denied."
     end
 end
